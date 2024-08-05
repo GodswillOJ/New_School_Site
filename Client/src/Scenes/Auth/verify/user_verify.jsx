810a8911ded19f25a002b1143a5140ec7f68fadd
GodswillOJ <110397088+GodswillOJ@users.noreply.github.com>
2024-08-05 18:48:37 +0100

    jkb

--- Client/src/Scenes/Auth/verify/user_verify.jsx
+++ Client/src/Scenes/Auth/verify/user_verify.jsx
@@ -1,42 +1,39 @@
 import React, { useState } from 'react';
-import  { Link, useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom';
 import { useGetUserVerifyQuery } from 'state/api';
 
 const UserVerify = () => {
-    const { data, isLoading } = useGetUserVerifyQuery();
-    const [message, setUserMessage] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const { id } = useParams();  // Get the id from the URL
+  const { data, isLoading, error } = useGetUserVerifyQuery(id);  // Pass the id to the query
+  const [message, setUserMessage] = useState('');
 
-    console.log('user_data: ', data)
-  
-  
-    if (loading) {
-      return <div>Loading...</div>;
-    }
-  
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    }
-  
-    return (
-      <div id="VerifyCont">
-        <div id="Verify_info">
-          <h1>Student Verification Mail</h1>
-          <div>
-            {data ? (
-              <p>
-                <b>Hi, {data.username}</b>
-                {message} please click to redirect to login <Link to='/login'>proceed to login</Link>
-              </p>
-            ) : (
-              <p>User data not available</p>
-            )}
-          </div>
+  console.log('user_data:', data);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div id="VerifyCont">
+      <div id="Verify_info">
+        <h1>Student Verification Mail</h1>
+        <div>
+          {data ? (
+            <p>
+              <b>Hi, {data.username}</b>
+              {message} please click to redirect to login <Link to='/login'>proceed to login</Link>
+            </p>
+          ) : (
+            <p>User data not available</p>
+          )}
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default UserVerify;
-  
+export default UserVerify;
