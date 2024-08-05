edc66b162927936517ce692ba8ce0b5f1a9e2bd3
GodswillOJ <110397088+GodswillOJ@users.noreply.github.com>
2024-08-05 16:16:50 +0100

    hv

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,6 @@ import path from 'path';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import helmet from 'helmet';
-import bodyParser from 'body-parser';
 import Teachers from './src/routes/teacher.js';
 import General from './src/routes/general.js';
 import Management from './src/routes/management.js';
@@ -24,8 +23,6 @@ app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); // Ensure this is set to true
 
 // Serve static files from the "public" directory
 app.use('/public', express.static(path.join(__dirname, 'public')));
